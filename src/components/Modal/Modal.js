f7f923d2afc5998c7a8a26bdee1859ccd0a605c6
Mir Sujat <mir.sujat@gmail.com>
2021-07-05 17:00:29 +0600

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,6 +5,26 @@ import Portal from "./Portal";
 
 
 class Modal extends Component {
+
+/*
+   * @constructor
+   * @desc Dialog object providing modal focus management.
+   *
+   * Assumptions: The element serving as the dialog container is present in the
+   * DOM and hidden. The dialog container has role='dialog'.
+   *
+   * @param dialogId
+   *          The ID of the element serving as the dialog container.
+   * @param focusAfterClosed
+   *          Either the DOM node or the ID of the DOM node to focus when the
+   *          dialog closes.
+   * @param focusFirst
+   *          Optional parameter containing either the DOM node or the ID of the
+   *          DOM node to focus when the dialog opens. If not specified, the
+   *          first focusable element in the dialog will receive focus.
+   */
+
+
   render() {
     console.log("Props From Modal: ", this.props);
     let content = null;
