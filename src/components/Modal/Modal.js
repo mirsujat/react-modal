e1b9a969a73bb9191cf204d00421d07ae59cfb74
Mir Sujat <mir.sujat@gmail.com>
2021-07-09 19:42:46 +0600

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,74 +1,264 @@
 import React, { Component } from 'react';
-import Backdrop from "./Backdrop/Backdrop";
-import Portal from "./Portal";
+import FocusTrap from 'focus-trap-react';
+import portal from "./Portal";
 
 
 
 
 class Modal extends Component {
- 
-  constructor(props) {
-    super(props);
-  
-  
+ static defaultProps = {
+    underlayProps: {},
+    dialogId: 'modal-dialog',
+    underlayClickExits: true,
+    escapeExits: true,
+    underlayColor: 'rgba(0,0,0,0.5)',
+    includeDefaultStyles: true,
+    focusTrapPaused: false,
+    
+  };
+
+  componentWillMount() {
+    if (!this.props.titleText && !this.props.titleId) {
+      throw new Error(
+        'Modal instances should have a `titleText` or `titleId`'
+      );
+    }
   }
 
-   
-//    * @desc Dialog object providing modal focus management.
-//    *
-//    * Assumptions: The element serving as the dialog container is present in the
-//    * DOM and hidden. The dialog container has role='dialog'.
-//    *
-//    * @param dialogId
-//    *          The ID of the element serving as the dialog container.
-//    * @param focusAfterClosed
-//    *          Either the DOM node or the ID of the DOM node to focus when the
-//    *          dialog closes.
-//    * @param focusFirst
-//    *          Optional parameter containing either the DOM node or the ID of the
-//    *          DOM node to focus when the dialog opens. If not specified, the
-//    *          first focusable element in the dialog will receive focus.
+  componentDidMount() {
+    if (this.props.onEnter) {
+      this.props.onEnter();
+    }
+
+    // Timeout to ensure this happens *after* focus has moved
+    const applicationNode = this.getApplicationNode();
+    setTimeout(() => {
+      if (applicationNode) {
+        applicationNode.setAttribute('aria-hidden', 'true');
+      }
+    }, 0);
+
+    if (this.props.escapeExits) {
+      this.addKeyDownListener();
+    }
+
+
+  }
+
+  componentDidUpdate(prevProps) {
+
+    if (this.props.escapeExits && !prevProps.escapeExits) {
+      this.addKeyDownListener();
+    } else if (!this.props.escapeExits && prevProps.escapeExits) {
+      this.removeKeyDownListener();
+    }
+  }
+
+  componentWillUnmount() {
  
+    const applicationNode = this.getApplicationNode();
+    if (applicationNode) {
+      applicationNode.setAttribute('aria-hidden', 'false');
+    }
+    this.removeKeyDownListener();
+  }
 
+  addKeyDownListener() {
+    setTimeout(() => {
+      document.addEventListener('keydown', this.checkDocumentKeyDown);
+    });
+  }
 
+  removeKeyDownListener() {
+    setTimeout(() => {
+      document.removeEventListener('keydown', this.checkDocumentKeyDown);
+    });
+  }
 
+  getApplicationNode = () => {
+    if (this.props.getApplicationNode) return this.props.getApplicationNode();
+    return this.props.applicationNode;
+  };
+
+  checkUnderlayClick = event => {
+    if (
+      (this.dialogNode && this.dialogNode.contains(event.target)) ||
+      // If the click is on the scrollbar we don't want to close the modal.
+      event.pageX > event.target.ownerDocument.documentElement.offsetWidth ||
+      event.pageY > event.target.ownerDocument.documentElement.offsetHeight
+    )
+      return;
+    this.exit(event);
+  };
+
+  checkDocumentKeyDown = event => {
+    if (
+      this.props.escapeExits &&
+      (event.key === 'Escape' || event.key === 'Esc' || event.keyCode === 27)
+    ) {
+      this.exit(event);
+    }
+  };
+
+  exit = event => {
+    if (this.props.onExit) {
+      this.props.onExit(event);
+    }
+  };
 
   render() {
-    
-    console.log("Props From Modal: ", this.props);
+    const props = this.props;
 
-    let content = null;
-    let modalClass = "modal-wrapper modal-hidden";
-    if (this.props.activateModal) {
-      modalClass = "modal-wrapper has-modal";
+    let style = {};
+    if (props.includeDefaultStyles) {
+      style = {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: 1050,
+        overflowX: 'hidden',
+        overflowY: 'auto',
+        WebkitOverflowScrolling: 'touch',
+        textAlign: 'center'
+      };
+
+      if (props.underlayColor) {
+        style.background = props.underlayColor;
+      }
+
+      if (props.underlayClickExits) {
+        style.cursor = 'pointer';
+      }
     }
-   
 
-    if (this.props.activateModal) {
-      content = ( 
-        <div
-        activatemodal={this.props.activateModal}
-      >      
-        <div className={modalClass}>
-          <div className="modal" >
-            <div>{this.props.children}</div>
-          </div>
-           <Backdrop  />
-           </div>
-        </div>
+    if (props.underlayStyle) {
+      for (const key in props.underlayStyle) {
+        if (!props.underlayStyle.hasOwnProperty(key)) continue;
+        style[key] = props.underlayStyle[key];
+      }
+    }
+
+    const underlayProps = {
+      className: props.underlayClass,
+      style: style
+    };
+
+    if (props.underlayClickExits) {
+      underlayProps.onMouseDown = this.checkUnderlayClick;
+    }
+
+    for (const prop in this.props.underlayProps) {
+      underlayProps[prop] = this.props.underlayProps[prop];
+    }
+
+    let verticalCenterStyle = {};
+    if (props.includeDefaultStyles) {
+      verticalCenterStyle = {
+        display: 'inline-block',
+        height: '100%',
+        verticalAlign: 'middle'
+      };
+    }
+
+    const verticalCenterHelperProps = {
+      key: 'a',
+      style: verticalCenterStyle
+    };
+
+    let dialogStyle = {};
+    if (props.includeDefaultStyles) {
+      dialogStyle = {
+        display: 'inline-block',
+        textAlign: 'left',
+        top: 0,
+        maxWidth: '100%',
+        cursor: 'default',
+        outline: props.focusDialog ? 0 : null
+      };
+
+      if (props.verticallyCenter) {
+        dialogStyle.verticalAlign = 'middle';
+        dialogStyle.top = 0;
+      }
+    }
+
+    if (props.dialogStyle) {
+      for (const key in props.dialogStyle) {
+        if (!props.dialogStyle.hasOwnProperty(key)) continue;
+        dialogStyle[key] = props.dialogStyle[key];
+      }
+    }
+
+    const dialogProps = {
+      key: 'b',
+      ref: function(el) {
+        this.dialogNode = el;
+      }.bind(this),
+      role: props.alert ? 'alertdialog' : 'dialog',
+      id: props.dialogId,
+      className: props.dialogClass,
+      style: dialogStyle
+    };
+    if (props.titleId) {
+      dialogProps['aria-labelledby'] = props.titleId;
+    } else if (props.titleText) {
+      dialogProps['aria-label'] = props.titleText;
+    }
+    if (props.focusDialog) {
+      dialogProps.tabIndex = '-1';
+    }
+
+    // Apply data- and aria- attributes passed as props
+    for (let key in props) {
+      if (/^(data-|aria-)/.test(key)) {
+        dialogProps[key] = props[key];
+      }
+    }
+
+    const childrenArray = [
+      React.createElement('div', dialogProps, props.children)
+    ];
+
+    if (props.verticallyCenter) {
+      childrenArray.unshift(
+        React.createElement('div', verticalCenterHelperProps)
       );
     }
 
+    const focusTrapOptions = props.focusTrapOptions || {};
+    if (props.focusDialog || props.initialFocus) {
+      focusTrapOptions.initialFocus = props.focusDialog
+        ? `#${this.props.dialogId}`
+        : props.initialFocus;
+    }
+    focusTrapOptions.escapeDeactivates = props.escapeExits;
 
-   
-    return (
-    <Portal>
-       <div>{content}</div>
-    </Portal>
-       
-  
+    //TODO
+    // const Element = <FocusTrap {...focusTrapOptions} paused={props.focusTrapPaused} >
+    //   <div {...underlayProps}>
+    //   {childrenArray}
+    //   </div>
+    // </FocusTrap>;
+
+
+    return React.createElement(
+      FocusTrap,
+      {
+        focusTrapOptions,
+        paused: props.focusTrapPaused
+      },
+      React.createElement('div', underlayProps, childrenArray)
     );
   }
 }
+
+const RenderModal = portal(Modal);
+
+RenderModal.renderTo = function(AppNode) {
+  return portal(Modal, { renderTo: AppNode });
+};
+
 
 export default Modal;
