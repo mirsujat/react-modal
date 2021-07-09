c2eaf92576120ead549330527c566b5236059398
Mir Sujat <mir.sujat@gmail.com>
2021-07-09 19:43:09 +0600

    Update Portal.js

--- src/components/Modal/Portal.js
+++ src/components/Modal/Portal.js
@@ -1,24 +1,58 @@
-import { Component } from "react";
+import React,{ Component } from "react";
 import ReactDOM from "react-dom";
 
-class Portal extends Component {
-  constructor(props) {
-    super(props);
-    this.container = document.createElement("div");
-    this.container.setAttribute("id", "react-modal-application");
 
+const portal = (WrappedComponent, options) =>{
+  if(!window.document){
+    return class EmptyPortal extends Component{
+      render(){
+        return false; 
+      }
+    }
   }
+  options = options || {};
 
-  componentDidMount() {
-    document.body.appendChild(this.container);
-  }
-  componentWillUnmount() {
-    document.body.removeChild(this.container);
-  }
+  class Portal extends Component{
+    static defaultProps = {
+      mounted : true
+    };
 
-  render() {
-    return ReactDOM.createPortal(this.props.children, this.container);
-  }
+    static WrappedComponent = WrappedComponent;
+
+    componentDidMount = () =>{
+      if(this.props.mounted){
+          this.container = (() => {
+    
+        if (!options.renderTo) {
+          var result = document.createElement('div');
+          document.body.appendChild(result);
+          return result;
+        } else if (typeof options.renderTo === 'string') {
+          return document.querySelector(options.renderTo);
+        } else {
+          return options.renderTo;
+        }
+      })();
+        }
+    }
+  
+    componentWillUnmount = () =>{
+      if (!options.renderTo) {
+        this.container.parentNode.removeChild(this.container);
+      }
+      ReactDOM.unmountComponentAtNode(this.container);
+    }
+    renderElement = () =>{
+      return React.createElement(WrappedComponent, this.props, this.props.children)
+    }
+  
+
+    render(){
+      if(this.props.mounted === false) return null;
+      return ReactDOM.createPortal(this.renderElement, this.container);
+    }
+    
+  } 
+  return Portal;
 }
-
-export default Portal;
+export default portal;
