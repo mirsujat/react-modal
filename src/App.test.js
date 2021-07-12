ac4d8a21b395eadd472b045f13989261d93bb4a1
Mir Sujat <mir.sujat@gmail.com>
2021-07-12 20:01:10 +0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render App Component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Open modal/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('should render Modal Component', async() => {
+  render(<App />);
+  fireEvent.click(screen.getByText('Open modal'))
+  await waitFor(() => screen.getByRole('dialog'))
+expect(screen.getByRole('dialog')).toHaveTextContent('This is an accessable modal')
 });
